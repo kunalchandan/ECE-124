fbc9e3e92ba5ede894b41f8e9aea12a4521482fc
Kunal Chandan <kunal_chandan@outlook.com>
2019-05-28 11:16:25 -0400

    Lab2 - PostLab ALU w/out adder

--- Lab2/LogicalStep_Lab2_top.vhd
+++ Lab2/LogicalStep_Lab2_top.vhd
@@ -25,6 +25,25 @@ architecture SimpleCircuit of LogicalStep_Lab2_top is
    ); 
    end component;
 	
+	component segment7_mux port (
+	clk	: in std_logic :='0';
+	DIN2	: in std_logic_vector(6 downto 0);
+	DIN1	: in std_logic_vector(6 downto 0);
+	DOUT	: out std_logic_vector(6 downto 0);	
+	DIG2	: out std_logic;
+	DIG1	: out std_logic
+	);
+	end component;
+	
+	component segment7_alu port (
+	OPIN1		: in std_logic_vector(3 downto 0);
+	OPIN2		: in std_logic_vector(7 downto 4);
+	OPERATOR	: in std_logic_vector(3 downto 0);
+	LOUT		: out std_logic_vector(7 downto 0)
+--	AOUTLed	: out std_logic_vector(7 downto 0);
+--	AOUTSeg7	: out std_logic_vector(6 downto 0)
+	);
+	end component;
 	
 -- Create any signals, or temporary variables to be used
 --
@@ -32,13 +51,27 @@ architecture SimpleCircuit of LogicalStep_Lab2_top is
 --
 	signal seg7_A		: std_logic_vector(6 downto 0);
 	signal hex_A		: std_logic_vector(3 downto 0);
-	
+	signal seg7_B		: std_logic_vector(6 downto 0);
+	signal hex_B		: std_logic_vector(7 downto 4);
+	signal pb_bar		: std_logic_vector(3 downto 0);
+	--signal Led_OUT		: std_logic_vector(7 downto 0);
 	
 -- Here the circuit begins
 
 begin
-
-
+	
+	-- the two digits
+	hex_A <= sw(3 downto 0);
+	hex_B <= sw(7 downto 4);
+	-- invert push buttons
+	pb_bar <= NOT(pb);
+	
+	
+-- Instances (Component)
+	INST1: SevenSegment port map (hex_A, seg7_A);
+	INST2: SevenSegment port map (hex_B, seg7_B);
+	INST3: segment7_mux port map(clkin_50, seg7_A, seg7_B, seg7_data, seg7_char2, seg7_char1); 
+	INST4: segment7_alu port map (hex_A, hex_B, pb_bar, leds);
 
  
 end SimpleCircuit;
