8baf4662efaf7c974e42b62baf7e6f037d1a74c6
Kunal Chandan <kunal_chandan@outlook.com>
2019-07-13 18:35:31 -0400

    added 4-bit shifter and counter

--- Lab4/Bidir_shift_reg.vhd
+++ Lab4/Bidir_shift_reg.vhd
@@ -7,36 +7,38 @@ Entity Bidir_shift_reg is port
 	 RESET_n      : in  std_logic := '0';
 	 CLK_EN       : in  std_logic := '0'; 
 	 LEFTO_RIGHT1 : in  std_logic := '0';
-	 REG_BITS     : out std_logic_vector(7 downto 0)
+	 REG_BITS     : out std_logic_vector(3 downto 0)
 ); 
 end Entity; 
 
 ARCHITECTURE one OF Bidir_shift_reg IS 
-    signal sreg : std_logic_vector(7 downto 0); 
+    signal sreg : std_logic_vector(3 downto 0); 
 
 BEGIN 
 
 process (CLK, RESET_n) is 
 begin 
     if (RESET_n = '0') then 
-	     sreg <= "00000000"; 
+	     sreg <= "0000"; 
     
 	 elsif (rising_edge(CLK) AND (CLK_EN = '1')) then 
 	     
 		  if (LEFTO_RIGHT1 = '1') then -- TRUE for RIGHT shift 
 		      -- Removes the rightmost bit, and appends a '1' to the left
 				
-		      sreg (7 downto 0) <= '1' & sreg(7 downto 1); -- right-shift of bits 
+		      sreg (3 downto 0) <= '1' & sreg(3 downto 1); -- right-shift of bits 
 		  
 		  elsif (LEFTO_RIGHT1 = '0') then 
 		      -- Removes the leftmost bit, and appends a '0' to the right
-		      sreg (7 downto 0) <= sreg(6 downto 0) & '0'; -- left-shift of bits 
+		      sreg (3 downto 0) <= sreg(2 downto 0) & '0'; -- left-shift of bits 
 	
 	  end if; 
 	
 	end if;
 
 end process; 
-REG_BITS <= sreg; 
+	
+	-- Output new shifted vector;
+	REG_BITS <= sreg; 
 
 END one;
